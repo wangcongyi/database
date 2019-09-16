259aee4844c8d72cde494ecb09a434df4b589e78
王聪奕 <360805958@qq.com>
2019-09-16 16:00:20 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./queries')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.json({ info: 'test database with node and postgreSQL' })
+})
+
+app.get('/user', db.getUsers)
+app.get('/user/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+
+app.listen(3000, () => {
+  console.log('App running on port 3000')
+})
